3517fffebe36ad06088dddf57c6e79232bc9784e
Plankton5544 <planktonweipert@gmail.com>
2025-12-17 17:15:18 +0000

    Worked on C physics ascii simulator, may devlop into couch-co-op game

--- Cscripts/game/main.c
+++ Cscripts/game/main.c
@@ -1,80 +1,227 @@
-#include <ncurses.h>
-#include <time.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdbool.h>
 
-void apply_phys() {
-    player1.y = player1.y - gravity;
-    //f=ma
+#include <termios.h>
+#include <fcntl.h>
 
-    velocity =
-    //move.x = speed * cos(angle * PI / 180);
-    //move.y = speed * sin(angle * PI / 180);
+struct termios old_term;
 
+struct term {
+    int column;
+    int row;
 };
 
-
-struct velocity {
-    float
-}
-struct position {
+struct pos {
     float x;
     float y;
 };
 
-struct player {
-    float mass;
-
+struct vel {
+    float vx;
+    float vy;
 };
 
-int main() {
-    initscr();
-    noecho();
-    cbreak();
-    keypad(stdscr, TRUE);
-    nodelay(stdscr, TRUE);  // <-- Add this! Makes getch() non-blocking
+struct entity {
+    struct pos position;
+    struct vel velocity;
+};
 
-    int height, width;
-    getmaxyx(stdscr, height, width);
+//For Non-Blocking Inpu
+void enable_raw_mode(void) {
+    struct termios new_term;
 
-    struct position player1;
-    player1.x = width / 2;
-    player1.y = height / 2;
+    tcgetattr(STDIN_FILENO, &old_term);
+    new_term = old_term;
 
-    int frame_count = 0;
+    new_term.c_lflag &= ~(ICANON | ECHO);
+    new_term.c_cc[VMIN] = 0;
+    new_term.c_cc[VTIME] = 0;
 
-    while (1) {
-        int input = getch();
-        if (input != ERR) {  // ERR means no key was pressed
-            switch (input) {
-                case 'w': player1.y--; player1.y--; break;
-                case 's': player1.y++; break;
-                case 'a': player1.x--; break;
-                case 'd': player1.x++; break;
-                case 'q':
-          endwin();
-          return 0;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
+
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+}
+
+//Disables Non-Blocking Input
+void disable_raw_mode(void) {
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
+}
+
+void render(int height, int width, struct entity *object, int size) {
+    printf("\033[H");  // Move cursor to top-left
+    char screen[height][width];
+        for (int y=0; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                if (x == width-1 || x == 0) {
+                    screen[y][x] = '|';
+                }else if (y == height-1 || y == 0) {
+                    screen[y][x] = '=';
+                }else {
+                    screen[y][x] = ' ';
+                }
             }
         }
 
-        // Clear and redraw
-        clear();
-
-        for (int y = 2; y < height - 2; y++) {
-            for (int x = 2; x < width - 2; x++) {
-                mvprintw(y, x, ".");
-            }
+    for (int z=0; z<size; z++) {
+        //static struct pos pastpos[50];
+        //int length = sizeof(pastpos) / sizeof(pastpos[0]);
+        struct entity obj = object[z];
+        //for (int i=length-1; i>0; i--) {
+        //    pastpos[i].x = pastpos[i-1].x;
+        //    pastpos[i].y = pastpos[i-1].y;
+        //}
+        //pastpos[0].x = (int)obj.position.x;
+        //pastpos[0].y = (int)obj.position.y;
+        //for (int i=1; i<length; i++) {
+        //    int tx = (int)pastpos[i].x;
+        //    int ty = (int)pastpos[i].y;
+        //    if (tx >= 0 && tx < width && ty >= 0 && ty < height) {
+        //        screen[ty][tx] = '~';
+        //    }
+        //}
+        int dx = (int)obj.position.x;
+        int dy = (int)obj.position.y;
+        if (dx >= 0 && dx < width && dy >= 0 && dy < height) {
+            screen[dy][dx] = 'o';
         }
-
-
-        mvprintw(player1.y, player1.x, "P");
-        refresh();
-
-        struct timespec req = {0, 100000000}; // 100ms = 10 FPS
-        nanosleep(&req, NULL);
-
-        frame_count++;
     }
 
-    endwin();
+    for (int y=0; y<height; y++) {
+        for (int x=0; x<width; x++) {
+            putchar(screen[y][x]);
+        }
+        putchar('\n');
+    }
+}
+
+void phy_update(struct entity *object, int size, float gravity, float dt) {
+    for (int z=0; z<size; z++) {
+        struct entity *obj = &object[z];
+        obj->velocity.vy += (gravity * dt);
+        obj->position.x += (obj->velocity.vx * dt);
+        obj->position.y += (obj->velocity.vy * dt);
+    }
+}
+
+void collision(struct entity *object, int size, int height, int width, float restitution) {
+    for (int z=0; z<size; z++) {
+        struct entity *obj = &object[z];
+        //Vertical Impacts
+        if (obj->position.y >= height - 1) {
+            obj->position.y = height - 1;
+            obj->velocity.vy = -obj->velocity.vy * restitution;
+        }else if (obj->position.y <= 1) {
+            obj->position.y = 1;
+            obj->velocity.vy = -obj->velocity.vy * restitution;
+        }
+
+        //Horizontal Impacts
+        if (obj->position.x <= 2 || obj->position.x >= width - 2) {
+            obj->velocity.vx = -obj->velocity.vx * restitution;
+        }
+
+        //Friction 2% loss Horizontally
+        if (obj->position.y >= height - 2) {
+            obj->velocity.vx *= 0.98f;
+        }
+    }
+}
+
+void handle_input(char c, struct entity *object, int size, bool *running) {
+    switch (c) {
+        case 'q':
+            *running = false;
+            break;
+        case 'a':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vx -= 10;
+            }
+            break;
+        case 'd':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vx += 10;
+            }
+            break;
+        case 'w':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vy -= 15;
+            }
+            break;
+        case 's':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vy += 15;
+            }
+            break;
+    }
+}
+
+
+int main() {
+    enable_raw_mode();
+    float gravity = 30.0f;
+    float restitution = 0.7f;
+    float dt = 0.05f;
+    struct entity ball[2];
+    int size=sizeof(ball) / sizeof(*ball);
+
+    ball[0].position.x=20;
+    ball[0].position.y=20;
+
+    ball[0].velocity.vx=20;
+
+    ball[1].position.x=10;
+    ball[1].position.y=10;
+
+    ball[1].velocity.vy=20;
+
+    ball[2].position.x=15;
+    ball[2].position.y=15;
+
+    ball[2].velocity.vx=1;
+
+    bool running=true;
+
+    // Get the terminal size
+    struct winsize sz;
+
+    printf("\033[2J"); // Clear terminal
+    printf("\033[?25l");
+
+    while (running) {
+        //Physics
+        phy_update(ball, size,  gravity, dt);
+
+        //Input
+        char c;
+        while (read(STDIN_FILENO, &c, 1) > 0) {
+            handle_input(c, ball, size, &running);
+        }
+
+        //Terminal Resize
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &sz);
+        struct term terminal={
+            .row = sz.ws_row-1,
+            .column = sz.ws_col
+        };
+
+        //Collision
+        collision(ball, size, terminal.row-1, terminal.column-1, restitution);
+
+        //RendeR
+        render(terminal.row, terminal.column, ball, size);
+
+        //Sleep
+        usleep(50000);
+    }
+
+    printf("\033[?25h");
+    disable_raw_mode();
     return 0;
 }
+
--- Cscripts/phy_ascii/main.c
+++ Cscripts/phy_ascii/main.c
@@ -3,7 +3,10 @@
 #include <sys/ioctl.h>
 #include <stdbool.h>
 
-#include <string.h>
+#include <termios.h>
+#include <fcntl.h>
+
+struct termios old_term;
 
 struct term {
     int column;
@@ -23,23 +26,75 @@ struct vel {
 struct entity {
     struct pos position;
     struct vel velocity;
+    struct pos pastpos[5];
+    int state;
 };
 
-void render(int height, int width, struct entity *obj) {
+void enable_raw_mode(void) {
+    struct termios new_term;
+
+    tcgetattr(STDIN_FILENO, &old_term);
+    new_term = old_term;
+
+    new_term.c_lflag &= ~(ICANON | ECHO);
+    new_term.c_cc[VMIN] = 0;
+    new_term.c_cc[VTIME] = 0;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
+
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+}
+
+void disable_raw_mode(void) {
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
+}
+
+void render(int height, int width, struct entity *object, int size) {
     printf("\033[H");  // Move cursor to top-left
     char screen[height][width];
-
     for (int y=0; y<height; y++) {
         for (int x=0; x<width; x++) {
-            screen[y][x] = ' ';
+            if (x == width-1 || x == 0) {
+                screen[y][x] = '|';
+            }else if (y == height-1 || y == 0) {
+                screen[y][x] = '=';
+            }else {
+                screen[y][x] = ' ';
+            }
         }
     }
 
-    int dx = (int)obj->position.x;
-    int dy = (int)obj->position.y;
 
-    if (dx >= 0 && dx < width && dy >= 0 && dy < height) {
-        screen[dy][dx] = 'O';
+    for (int z=0; z<size; z++) {
+        struct entity *obj = &object[z];
+
+        //struct pos pastpos[50];
+        int length = sizeof(obj->pastpos) / sizeof(obj->pastpos[0]);
+        for (int i=length-1; i>0; i--) {
+            obj->pastpos[i].x = obj->pastpos[i-1].x;
+            obj->pastpos[i].y = obj->pastpos[i-1].y;
+        }
+        obj->pastpos[0].x = (int)obj->position.x;
+        obj->pastpos[0].y = (int)obj->position.y;
+        for (int i=1; i<length; i++) {
+            int tx = (int)obj->pastpos[i].x;
+            int ty = (int)obj->pastpos[i].y;
+            if (tx >= 0 && tx < width && ty >= 0 && ty < height) {
+                screen[ty][tx] = '~';
+            }
+        }
+        int dx = (int)obj->position.x;
+        int dy = (int)obj->position.y;
+        if (dx >= 0 && dx < width && dy >= 0 && dy < height) {
+            if (obj->state == 1) {
+                screen[dy][dx] = 'o';
+            }else if (obj->state == 2) {
+                screen[dy][dx] = '0';
+            }else if (obj->state == 3) {
+                screen[dy][dx] = '@';
+            }
+        }
+
     }
 
     for (int y=0; y<height; y++) {
@@ -48,31 +103,174 @@ void render(int height, int width, struct entity *obj) {
         }
         putchar('\n');
     }
-
 }
 
-void phy_update(struct entity *obj, float gravity, float dt) {
-    obj->velocity.vy += (gravity * dt);
-    obj->position.x += (obj->velocity.vx * dt);
-    obj->position.y += (obj->velocity.vy * dt);
-}
+void phy_update(struct entity *object, int size, float gravity, float dt) {
+    for (int z=0; z<size; z++) {
+        struct entity *obj = &object[z];
+        obj->velocity.vy += (gravity * dt);
+        obj->position.x += (obj->velocity.vx * dt);
+        obj->position.y += (obj->velocity.vy * dt);
 
-void collision(struct entity *obj, int height, int width, float restitution) {
-    if (obj->position.y >= height - 1) {
-        obj->position.y = height - 1;
-        obj->velocity.vy = -obj->velocity.vy * restitution;
+        if ((obj->velocity.vx/obj->velocity.vy) <= 0.5) {
+            obj->state=1;
+        } else if ((obj->velocity.vx/obj->velocity.vy) <= 1) {
+            obj->state=2;
+        } else if ((obj->velocity.vx/obj->velocity.vy) >= 2) {
+            obj->state=3;
+        }
     }
-    if (obj->position.x <= 0 || obj->position.x >= width - 1) {
-        obj->velocity.vx = -obj->velocity.vx * restitution;
-    }
-
 }
+
+void collision(struct entity *object, int size, int height, int width, float restitution) {
+    for (int z=0; z<size; z++) {
+        struct entity *obj = &object[z];
+        //Vertical Impacts
+        if (obj->position.y >= height - 1) {
+            obj->position.y = height - 1;
+            obj->velocity.vy = -obj->velocity.vy * restitution;
+        }else if (obj->position.y <= 1) {
+            obj->position.y = 1;
+            obj->velocity.vy = -obj->velocity.vy * restitution;
+        }
+
+        if (obj->position.x <= 2) {
+            obj->position.x = 2;
+            obj->velocity.vx = -obj->velocity.vx * restitution;
+        } else if (obj->position.x >= width - 2) {
+            obj->position.x = width - 2;
+            obj->velocity.vx = -obj->velocity.vx * restitution;
+        }
+
+        //Friction 2% loss Horizontally
+        if (obj->position.y >= height - 2) {
+            obj->velocity.vx *= 0.98f;
+        }
+
+        //Ball-Ball Collisions
+        for (int j=z+1; j<size; j++) {  // Only check each pair once
+            struct entity *obj2= &object[j];
+            float dx = obj2->position.x - obj->position.x;
+            float dy = obj2->position.y - obj->position.y;
+            float distance_squared = dx*dx + dy*dy;
+
+            if (distance_squared < 2.25 && distance_squared > 0.001f) {
+                float distance = distance_squared / distance_squared;
+
+                // Normalize collision vector
+                float nx = dx / distance;
+                float ny = dy / distance;
+
+                // Separate the balls (push apart)
+                float overlap = 1.5f - distance;  // 1.5 is your collision radius
+                obj->position.x -= nx * overlap * 0.5f;
+                obj->position.y -= ny * overlap * 0.5f;
+                obj2->position.x += nx * overlap * 0.5f;
+                obj2->position.y += ny * overlap * 0.5f;
+
+                // Calculate relative velocity
+                float dvx = obj2->velocity.vx - obj->velocity.vx;
+                float dvy = obj2->velocity.vy - obj->velocity.vy;
+                float relative_velocity = dvx * nx + dvy * ny;
+
+                // Only bounce if approaching
+                if (relative_velocity < 0) {
+                    // Calculate and apply impulse
+                    float impulse = -(1.0f + restitution) * relative_velocity;
+                    obj->velocity.vx -= impulse * nx * 0.5f;
+                    obj->velocity.vy -= impulse * ny * 0.5f;
+                    obj2->velocity.vx += impulse * nx * 0.5f;
+                    obj2->velocity.vy += impulse * ny * 0.5f;
+                }
+            }
+        }
+
+    }
+}
+
+void handle_input(char c, struct entity *object, int size, bool *running, float *gravity, float *restitution) {
+    switch (c) {
+        case 'q':
+            *running = false;
+            break;
+        case 'a':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vx -= 10;
+            }
+            break;
+        case 'd':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vx += 10;
+            }
+            break;
+        case 'w':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vy -= 15;
+            }
+            break;
+        case 's':
+            for (int z=0; z<size; z++) {
+                struct entity *ball = &object[z];
+                ball->velocity.vy += 15;
+            }
+            break;
+        case 'g':
+            *gravity+=1;
+            break;
+        case 'v':
+            *gravity-=1;
+            break;
+        case 'f':
+            *restitution+=.1;
+            break;
+        case 'c':
+            *restitution-=.1;
+            break;
+    }
+}
+
 
 int main() {
+    enable_raw_mode();
     float gravity = 30.0f;
     float restitution = 0.7f;
     float dt = 0.05f;
-    struct entity ball={{15, 5}, {25, 25}};
+    struct entity ball[5];
+    int size=sizeof(ball) / sizeof(*ball);
+
+    ball[0].position.x=20;
+    ball[0].position.y=20;
+
+    ball[0].velocity.vx=20;
+    ball[0].velocity.vy=0;
+
+    ball[1].position.x=10;
+    ball[1].position.y=10;
+
+    ball[1].velocity.vy=20;
+    ball[1].velocity.vx=0;
+
+    ball[2].position.x=15;
+    ball[2].position.y=15;
+
+    ball[2].velocity.vx=1;
+    ball[2].velocity.vy=0;
+
+    ball[3].position.x=24;
+    ball[3].position.y=35;
+
+    ball[3].velocity.vx=1;
+    ball[3].velocity.vy=4;
+
+    ball[4].position.x=1;
+    ball[4].position.y=3;
+
+    ball[4].velocity.vx=0;
+    ball[4].velocity.vy=2;
+
 
     bool running=true;
 
@@ -83,20 +281,33 @@ int main() {
     printf("\033[?25l");
 
     while (running) {
-        phy_update(&ball, gravity, dt);
+        //Physics
+        phy_update(ball, size,  gravity, dt);
 
+        //Input
+        char c;
+        while (read(STDIN_FILENO, &c, 1) > 0) {
+            handle_input(c, ball, size, &running, &gravity, &restitution);
+        }
+
+        //Terminal Resize
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &sz);
         struct term terminal={
-           .row = sz.ws_row,
-           .column = sz.ws_col
+            .row = sz.ws_row-1,
+            .column = sz.ws_col
         };
 
-        collision(&ball, terminal.row, terminal.column, restitution);
+        //Collision
+        collision(ball, size, terminal.row-1, terminal.column-1, restitution);
 
-        render(terminal.row, terminal.column, &ball);
+        //RendeR
+        render(terminal.row, terminal.column, ball, size);
+
+        //Sleep
         usleep(50000);
     }
 
     printf("\033[?25h");
+    disable_raw_mode();
     return 0;
 }
